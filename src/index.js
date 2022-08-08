44a8cc0ee296cc381006d3db01d7c3628a96df07
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-08-08 17:50:05 +0300

    almost done

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import name from './module-1';
+import './css/common.scss';
+
+console.log(name);
+console.log('This is index.html');
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ],
+  },
+  devServer: {
+    port: 4444,
+    open: true,
+  },
+  plugins: [new HtmlWebpackPlugin({template: 'src/index.html'})],
+};
