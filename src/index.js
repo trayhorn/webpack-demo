5d459145a41db4efe1bbd2c6b83bf31c181b71dc
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-08-19 13:35:00 +0300

    color-switch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
-import name from './module-1';
 import './css/common.scss';
 
-console.log(name);
-console.log('This is index.html');
+const colors = [
+  '#48FFF9',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const bodyEl = document.body;
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+
+
+let intervalId = null;
+
+startBtn.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)]
+  }, 1000);
+  startBtn.setAttribute('disabled', true);
+})
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled');
+})
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+
+
+
+
+
+
+
+
